0752b892b09bbf70175110ec78cf55b12645525d
Tanvir <zishantanvir05@gmail.com>
2025-02-11 12:50:27 +0600

    post function done

--- src/components/AddNewCampaign/AddNewCampaign.jsx
+++ src/components/AddNewCampaign/AddNewCampaign.jsx
@@ -1,8 +1,130 @@
+import Swal from "sweetalert2";
 
 const AddNewCampaign = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const image = e.target.image.value;
+        const campaignTitle = e.target.campaignTitle.value;
+        const campaignType = e.target.campaignType.value;
+        const amount = e.target.amount.value;
+        const Deadline = e.target.Deadline.value;
+        const campaignData = {image, campaignTitle, campaignType, amount, Deadline};
+        fetch("http://localhost:5000/campaigns",{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(campaignData)
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                Swal.fire({
+                    title: "Campaign Added Successfully",
+                    icon: "success",
+                    draggable: true
+                  })
+                e.target.reset()
+            }
+        })
+    };
+
     return (
         <div>
-            Add New Campaign            
+            <h2 className="text-center text-3xl my-5 font-semibold">
+                Add New Campaign
+            </h2>
+            <div className="hero bg-base-200 ">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
+                        <form className="card-body " onSubmit={handleSubmit}>
+                            <div className="form-control ">
+                                <label className="label my-2 block ">
+                                    <span className="label-text text-xl text-black">
+                                        Image Url
+                                    </span>
+                                </label>
+                                <input
+                                    type="text"
+                                    name="image"
+                                    placeholder="Campaign Image URL"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label my-2 block ">
+                                    <span className="label-text text-xl text-black">
+                                        Campaign title
+                                    </span>
+                                </label>
+                                <input
+                                    type="text"
+                                    name="campaignTitle"
+                                    placeholder="Campaign title"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="">
+                                <label className="label my-2 block ">
+                                    <span className="label-text text-xl text-black ">
+                                        Campaign type
+                                    </span>
+                                </label>
+                                <select className="select w-full max-w-xs" name="campaignType">
+                                    <option disabled selected>
+                                        Select Campaign type
+                                    </option>
+                                    <option value="personal issue">
+                                        Personal Issue
+                                    </option>
+                                    <option value="startup">Startup</option>
+                                    <option value="business">Business</option>
+                                    <option value="creative ideas">
+                                        Creative Ideas
+                                    </option>
+                                </select>
+                            </div>
+                            <div className="form-control">
+                                <label className="label my-2 block ">
+                                    <span className="label-text text-xl text-black">
+                                        Minimum donation amount
+                                    </span>
+                                </label>
+                                <input
+                                    type="text"
+                                    name="amount"
+                                    placeholder="Write Description"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label my-2 block ">
+                                    <span className="label-text text-xl text-black">
+                                        Deadline
+                                    </span>
+                                </label>
+                                <input
+                                    type="date"
+                                    name="Deadline"
+                                    placeholder="Write Description"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">
+                                    Add Campaign
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
