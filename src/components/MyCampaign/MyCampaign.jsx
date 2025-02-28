b7adffc4f2f10429c140e144b4ba4ec0318adf26
Tanvir <zishantanvir05@gmail.com>
2025-02-28 11:22:41 +0600

    deleted function added

--- src/components/MyCampaign/MyCampaign.jsx
+++ src/components/MyCampaign/MyCampaign.jsx
@@ -1,5 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { authContext } from "../AuthProvider/AuthProvider";
+import toast from "react-hot-toast";
+import Swal from "sweetalert2";
 
 const MyCampaign = () => {
     const { user } = useContext(authContext);
@@ -10,6 +12,30 @@ const MyCampaign = () => {
             .then((data) => setCampaign(data));
     }, [user?.email]);
     console.log("campaign data", campaign);
+
+    const handleDelete = (id) =>{
+        fetch(`http://localhost:5000/campaign/${id}`,{
+            method:"DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+
+             // Remove the deleted campaign from the state
+             setCampaign(campaign.filter((camp) => camp._id !== id))
+
+            if(data.deletedCount>0){
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Campaign Deleted Successful",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
+    }
+
     return (
         <div>
             <h3 className="text-center m-4 text-2xl font-semibold">My All Campaign - {campaign.length}</h3>
@@ -22,15 +48,20 @@ const MyCampaign = () => {
                             <th>Campaign Title</th>
                             <th>Campaign Type</th>
                             <th>amount</th>
+                            <th>Options</th>
                         </tr>
                     </thead>
                     <tbody>
                         {campaign.map((camp,idx) => (
-                            <tr className="bg-base-200">
+                            <tr key={idx} className="bg-base-200">
                                 <td>{1+idx}</td>
                                 <td>{camp.campaignTitle}</td>
                                 <td>{camp.campaignType}</td>
                                 <td>{camp.amount}</td>
+                                <td>
+                                    <button className="btn btn-error text-white" onClick={()=> handleDelete(camp._id)}>Delete</button>
+                                    <button className="mx-3 btn bg-blue-600 text-white">Edit</button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
---
+++ src/components/UpdateCampaign/UpdateCampaign.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCampaign = () => {
+  return (
+    <div>
+      <h3>This is Update Campaign</h3>
+    </div>
+  );
+};
+
+export default UpdateCampaign;
